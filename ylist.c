b9ae3b20e7ced717eb8123f2ad796a845048b217
Niyamaka <niyamaka@yuneta.io>
2021-01-03 16:57:03 +0100

    v4.5.0 Refactoring realms authz

--- ylist.c
+++ ylist.c
@@ -23,7 +23,7 @@
 #define NAME        "ylist"
 #define DOC         "List yunos"
 
-#define APP_VERSION     "4.4.1"
+#define APP_VERSION     "4.5.0"
 #define APP_DATETIME    __DATE__ " " __TIME__
 #define APP_SUPPORT     "<niyamaka at yuneta.io>"
 
